17d413181bf0d28151f34e442f258d89cba0cdbc
slongboro <susanlongley@gmail.com>
2019-10-14 16:19:30 +0100

    Raises an error if no bikes are available

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,17 +1,27 @@
 class DockingStation
   attr_reader :isBikeDocked, :bike
-  def initailize
+  def initialize
+    @bike_count = 1
     @isBikeDocked = true
   end
 
-  def release_bike()
-    @isBikeDocked = false
-    return Bike.new
+  def release_bike
+    if @bike_count == 1
+      @bike_count -= 1
+      @isBikeDocked = false
+      return Bike.new
+    else
+      raise "No Bikes are available"
+    end
   end
 
   def dock(bike)
+    @bike_count += 1
     @isBikeDocked = true
     @bike = bike
+  end
+  def test
+    return @bike_count
   end
 end
 
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -17,9 +17,15 @@ describe DockingStation do
 
   it 'Testing to see if docking is possible' do
     docking_station = DockingStation.new
-    bike = Bike.new
+    bike = docking_station.release_bike
     docking_station.dock(bike)
     expect(docking_station.isBikeDocked).to eq true
+  end
+
+  it 'Raises an error if no bikes are avaiable' do
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
+    expect{docking_station.release_bike}.to raise_error(RuntimeError, "No Bikes are available")
   end
 end
   #it 'asks if the bike is working?' do
