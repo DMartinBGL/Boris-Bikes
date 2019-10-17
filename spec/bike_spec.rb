59fa20f73debacf3bb7a131039ae21001b300a15
DMartinBGL <david.martin@bglgroup.co.uk>
2019-10-17 14:34:46 +0100

    ch18 broken method

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,11 +1,23 @@
 require "bike"
 
 describe Bike do
-  it "checks bike responds to working method" do
-    expect(Bike.new).to respond_to :working?
+  it 'creates and instance of bike' do
+    #subject = Bike.new
+    expect(subject).to be_a(Bike)
   end
 
-  # describe Bike do
-  #   it { is_expected.to respond_to :working? }
-  # end
+describe '#working?' do
+  it 'checks to see if bike is working' do
+    subject.working?
+    expect(subject.working?).to eq true
+  end
+end
+
+describe '#report_broken' do
+  it 'allows a bike to be reported as broken' do
+    subject.report_broken
+    expect(subject.report_broken).to eq true
+  end
+end
+
 end
